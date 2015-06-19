93b21fe50ecdf1bfe8f86642aff9b15d14d8b6d7
raulalvarezpinilla <raul_alvarez_pinilla@hotmail.com>
2015-06-19 16:11:04 +0200

    Added download-rootfs.sh

---
+++ filesystems/download-rootfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ROOTFSNAME=devstack_odl_ovs_KILO.qcow2
+
+# Download base rootfs
+echo "-----------------------------------------------------------------------"
+echo "Downloading rootfs..."
+vnx_download_rootfs -r ${BASEROOTFSNAME}.tgz
