71f751f2507193bd32e7cdabc53ef36e7ba9ac1a
raulalvarezpinilla <raul_alvarez_pinilla@hotmail.com>
2015-06-20 17:04:00 +0200

    Improved download script

--- filesystems/download-rootfs.sh
+++ filesystems/download-rootfs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Rootfs
 ROOTFSNAME=devstack_odl_ovs_KILO.qcow2
 
-# Download base rootfs
+# Move to the directory where the script is located
+CDIR=`dirname $0`
+cd $CDIR
+
+# Download rootfs
 echo "-----------------------------------------------------------------------"
 echo "Downloading rootfs..."
 vnx_download_rootfs -r ${ROOTFSNAME}.bz2
