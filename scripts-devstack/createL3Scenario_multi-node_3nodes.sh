d146c0fe6b5f628d6368bc7d2581b870b2664152
raulalvarezpinilla <raul_alvarez_pinilla@hotmail.com>
2015-07-02 12:06:52 +0200

    Changed order of creation

--- scripts-devstack/createL3Scenario_multi-node_3nodes.sh
+++ scripts-devstack/createL3Scenario_multi-node_3nodes.sh
@@ -11,15 +11,19 @@ neutron net-create NET1 --tenant-id $TENANT_ID --provider:network_type gre --pro
 neutron subnet-create --name SUBNET1 --tenant-id $TENANT_ID NET1 10.101.0.0/24 --gateway=10.101.0.1 --dns-nameserver 8.8.8.8
 NET1_ID=$(neutron net-list | grep -w NET1 | awk '{print $2}')
 
+nova boot VM-11 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET1_ID} --availability-zone nova:ctrl-comp1
+nova boot VM-12 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET1_ID} --availability-zone nova:comp2
+
 neutron net-create NET2 --tenant-id $TENANT_ID --provider:network_type gre --provider:segmentation_id 102
 neutron subnet-create --name SUBNET2 --tenant-id $TENANT_ID NET2 10.102.0.0/24 --gateway=10.102.0.1 --dns-nameserver 8.8.8.8
-NET2_ID=$(neutron net-list | grep -w NET2 | awk '{print $2}') 
+NET2_ID=$(neutron net-list | grep -w NET2 | awk '{print $2}')
+
+nova boot VM-22 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET2_ID} --availability-zone nova:ctrl-comp1
+nova boot VM-22 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET2_ID} --availability-zone nova:comp2
 
 neutron router-create R1 --tenant-id $TENANT_ID
 neutron router-interface-add R1 SUBNET1
 neutron router-interface-add R1 SUBNET2
 
-nova boot VM-11 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET1_ID} --availability-zone nova:ctrl-comp1
-nova boot VM-12 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET1_ID} --availability-zone nova:comp2
-nova boot VM-22 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET2_ID} --availability-zone nova:ctrl-comp1
-nova boot VM-22 --poll --flavor m1.tiny --image $IMAGE_ID --nic net-id=${NET2_ID} --availability-zone nova:comp2
+
+
